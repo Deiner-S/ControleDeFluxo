538ba422cce101e320a4c5a06d24ac4b0651790d
Deiner Rodrigues de Souza <deinersouza.tmsi@gmail.com>
2024-07-13 13:13:00 -0300

    Correção antes do envio

--- src/Classes/Main.java
+++ src/Classes/Main.java
@@ -10,17 +10,17 @@ public class Main {
 		
 		Scanner scan = new Scanner(System.in);
 		String entrada = "sim";
-		String novamente = "";
+		String outro = " seu ";
 		System.out.println("Ola! bem vindo ao sistema de verificação de notas!");
 		
 		while(entrada.equalsIgnoreCase("sim")) {
-			System.out.println("Gostaria de verificar"+novamente+"se seus alunos foram aprovados sim ou não?");
+			System.out.println("Gostaria de verificar se"+outro+"aluno foi aprovado, reprovado ou se está de recuperação? Sim ou não? ");
 			entrada = scan.next();
 			if(entrada.equalsIgnoreCase("sim")) {
 				situacaoAcademica();
-				novamente = " novamente ";
+				outro = " outro ";
 			}else if(entrada.equalsIgnoreCase("não") || entrada.equalsIgnoreCase("nao") ) {
-				System.out.println("Obrigado! Caso precise verificar suas notas e só me chamar!");
+				System.out.println("Obrigado! Caso precise verificar alguma nota e só me chamar!");
 				break;
 			}else {
 				System.out.println("Não entendi! Foi digitado algo diferente de sim e não\n");
@@ -37,24 +37,31 @@ public class Main {
 		
 		Scanner scan = new Scanner(System.in);
 		
+		Float media = 0.0f;
 		
+		Float nota = 0.0f;
 		
 		for(int i = 1;i<=4;i++) {
 			
-			System.out.println("insira o valor da "+i+"ª"+" nota");
-			Long nota = scan.nextLong();
+			System.out.println("insira o valor da "+i+"ª"+" nota\n");
+			nota = scan.nextFloat();
 			
-			if(nota>=7){				
-				System.out.print("Aluno aprovádo!\n\n");
-				
-			}else if(nota>=5 && nota<7) {				 
-				System.out.print("Aluno Recuperacao\n\n");
-				
-			}else {
-				System.out.print("Aluno Reprovado\n\n");
+			media += nota;
+			if(i == 4) {
+				media /= i;
 			}
 			
-			
+		}
+		
+		
+		if(media>=7){				
+			System.out.print("Aluno aprovádo!\nMedia:"+ media+"\n\n");
+				
+		}else if(media>=5 && media<7) {				 
+			System.out.print("Aluno Recuperacao\nMedia:"+ media+"\n\n");
+				
+		}else {
+				System.out.print("Aluno Reprovado\nMedia:"+ media+"\n\n");
 		}
 		
 		
